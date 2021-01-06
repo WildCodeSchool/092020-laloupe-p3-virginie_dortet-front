5bf8880435e4de29cb96ea599726eaf96fa8707f
toto <torphi78@gmail.com>
2021-01-06 11:15:19 +0100

    add components author

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.scss";
 import Auteur from "./components/Author/Author";
+import ContactForm from "./components/ContactForm/ContactForm";
 
 function App() {
   return (
     <div>
       <Auteur />
+      <ContactForm />
     </div>
   );
 }
