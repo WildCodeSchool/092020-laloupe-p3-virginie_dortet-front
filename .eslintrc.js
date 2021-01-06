6addae8ded8e7edf302ecdd679905dd349c28f92
Mustafa Topal <mustafatopalpro@gmail.com>
2021-01-06 14:31:16 +0100

    resolve conflit

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,11 +18,12 @@ module.exports = {
   },
   plugins: ["react"],
   rules: {
+    "no-alert": 0,
     "import/prefer-default-export": 0,
     "react/state-in-constructor": 0,
     "react/jsx-one-expression-per-line": 0,
     "no-console": 0,
     "react/jsx-indent": 0,
-    "react/no-unescaped-entities": 0
+    "react/no-unescaped-entities": 0,
   },
 };
