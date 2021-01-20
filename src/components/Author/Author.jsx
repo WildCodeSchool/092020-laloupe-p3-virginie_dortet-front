c1984195f84d61eb715245057552317ff3be3968
toto <torphi78@gmail.com>
2021-01-20 11:22:03 +0100

    author CSS

--- src/components/Author/Author.jsx
+++ src/components/Author/Author.jsx
@@ -9,7 +9,7 @@ function Author() {
   return (
     <div className="sectionAuteur">
       <Arbuste />
-      <h1 className="titrePage">L' Auteure</h1>
+      <h1 className="titrePageAuteur">L' Auteure</h1>
       <div className="flexybox">
         <div className="PhotoVirginie">
           <img
